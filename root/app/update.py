d905050bde050a2a6e2a2426853793f8634301be
Simora Altreus <simora@simora.haus.marklehaus.com>
2020-08-24 20:15:50 -0400

    updated with fallback url

--- root/app/update.py
+++ root/app/update.py
@@ -12,7 +12,7 @@ parser.add_argument("-c", "--check",
 parser.add_argument("-o", "--output", help="output directory to write downloaded file to")
 args = parser.parse_args()
 
-def get_releases(version = None):
+def get_release(version = None):
     if not version:
         url = "https://gitlab.com/api/v4/projects/7508674/releases/"
     else:
@@ -26,24 +26,31 @@ def get_releases(version = None):
         print("Failed to properly retrieve releases")
         sys.exit(1)
     try:
-        return json.loads(response.text)
+        return json.loads(response.text)['name']
     except Exception as e:
         print(f"Error: {str(e)}")
         sys.exit(1)
 
-if args.check:
-    releases = get_releases()
+def get_download_url(version):
     try:
-        soup = BeautifulSoup(releases[0]["description_html"], 'html.parser')
-    except Exception as e:
-        print(f"Error: {str(e)}")
+        response = requests.get(f"https://gitlab.com/api/v4/projects/7508674/releases/{version}")
+    except requests.exceptions.RequestException as e:
+        print("Failed to properly retrieve releases")
         sys.exit(1)
+    if response.status_code != 200:
+        print("Failed to properly retrieve releases")
+        sys.exit(1)
+
+    soup = BeautifulSoup(json.loads(response.text)["description_html"], 'html.parser')
     for link in soup.find_all('a'):
         if 'linux-amd64' in link.get('href'):
-            print(releases[0]["name"])
-            sys.exit(0)
-    print("Unable to find download")
-    sys.exit(1)
+            return link.get('href')
+    return f"https://sia.tech/releases/Sia-v{version}-linux-amd64.zip"
+
+
+if args.check:
+    print(get_release())
+    sys.exit(0)
 
 if not args.output:
     print("Missing output directory")
@@ -55,18 +62,13 @@ if not os.path.isdir(args.output):
 if not args.version or args.version.lower() == "latest":
     args.version = None
 
-releases = get_releases(args.version)
-if isinstance(releases, list):
-    soup = BeautifulSoup(releases[0]["description_html"], 'html.parser')
-else:
-    soup = BeautifulSoup(releases["description_html"], 'html.parser')
-for link in soup.find_all('a'):
-    if 'linux-amd64' in link.get('href'):
-        filename = link.get('href').split('/')[-1]
-        if not args.dry_run:
-            r = requests.get(link.get('href'), allow_redirects=True)
-            open(os.path.join(args.output, filename), 'wb').write(r.content)
-        print(os.path.join(args.output, filename))
-        sys.exit(0)
-print('Failed to find download')
-sys.exit(1)
+release = get_release(args.version)
+download_url = get_download_url(release)
+try:
+    r = requests.get(download_url, allow_redirects=True)
+    open(os.path.join(args.output, filename), 'wb').write(r.content)
+    print(os.path.join(args.output, filename))
+    sys.exit(0)
+except Exception as e:
+    print('Failed to find download')
+    sys.exit(1)
